6cf5a7cbc8c195ccf250324011443dbf6d1a744b
Gulshan Kumar <kumarjhagulshan@gmail.com>
2024-09-06 10:32:07 +0530

    skeleton UI prepared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+const mockUrls = [
+  "https://utfs.io/f/2658af96-7d5c-40e8-9f58-51cbeffc63e0-dz45mc.jpg",
+  "https://utfs.io/f/bb38714c-f3b4-4546-ab99-e30f287e466f-f2sgdl.jpg",
+  "https://utfs.io/f/e668bcad-91c0-442b-bec0-e7209f1e0a64-i2ezhv.jpg",
+  "https://utfs.io/f/cf63dade-c483-43a3-a849-b165c5b580d6-aav43h.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World
+    <main className="">
+      <div className="flex flex-wrap gap-5">
+        {mockImages.map((image) => {
+          return (
+            <img
+              alt="test"
+              src={image.url}
+              key={image.id}
+              className="w-48 object-contain"
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
