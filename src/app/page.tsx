9909b27dd980115ac1f4f30d14ea9f00fc88de0a
Gulshan Kumar <kumarjhagulshan@gmail.com>
2024-09-07 11:50:54 +0530

    connect clerk :

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany();
+
+  return (
+    <div className="flex flex-wrap gap-5">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="flex flex-col flex-wrap">
+          <div className="w-48 object-contain">
+            <img src={image.url} />
+          </div>
+          <div key={image.id}>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-5">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex flex-col flex-wrap">
-            <div className="w-48 object-contain">
-              <img src={image.url} />
-            </div>
-            <div key={image.id}>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div>SignIn Please</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
