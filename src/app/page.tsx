72a8b75b08ac0790a1bf7d24c1997ae629f50aa7
Gulshan Kumar <kumarjhagulshan@gmail.com>
2024-09-07 14:23:47 +0530

    minor refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,6 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-function MomoComponent() {
-  return (
-    <div className="flex flex-col content-center items-center justify-center gap-5">
-      <img
-        style={{ width: "500px", height: "500px", objectFit: "contain" }}
-        src="https://utfs.io/f/2039a314-0727-4654-98d2-1429a23199bb-1zykk.jpeg"
-      />
-      <span>I Love You My Momo ❤️</span>
-    </div>
-  );
-}
-
 async function Images() {
   const images = await db.query.images.findMany();
 
@@ -35,8 +23,7 @@ async function Images() {
 export default async function HomePage() {
   return (
     <main className="">
-      <MomoComponent />
-      <SignedOut></SignedOut>
+      <SignedOut>Please Sign In</SignedOut>
       <SignedIn>
         <Images />
       </SignedIn>
