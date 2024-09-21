cdf63d62d9d51e2fad744b334de148a09c91af8f
Gulshan Kumar <kumarjhagulshan@gmail.com>
2024-09-21 22:47:06 +0530

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
+      <html lang="en" className={`${GeistSans.variable} dark`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body suppressHydrationWarning>
           <div className="grid h-screen grid-rows-[auto,1fr]">
